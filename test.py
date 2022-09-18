e680d2300df57279df74ef826c30fc5223e19e16
Sreerag M <sreeragibtl@gmail.com>
2022-09-18 22:44:38 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ import requests
 
 model_inputs = {'prompt': 'Hello I am a [MASK] model.'}
 
-res = requests.post('http://localhost:8000/', json = model_inputs)
+res = requests.post('http://127.0. 0.1:8000/', json = model_inputs)
 
 print(res.json())
